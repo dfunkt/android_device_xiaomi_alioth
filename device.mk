2a845b8d0316f3958d526453b149f52382d459aa
Omkar Chandorkar <gotenksIN@aospa.co>
2023-11-30 21:39:52 +0900

    alioth: Enable mediaserver64 wrapper
    - Needed for mediaserver to work on 64bit-only with google modules
    
    Change-Id: I30d86f91ae6c00b3bb53910258380e808d96b38d
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- device.mk
+++ device.mk
@@ -235,6 +235,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml
 
+# Enable 64-bit mediaserver
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.mediaserver.64b.enable=true
+
 # Namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
